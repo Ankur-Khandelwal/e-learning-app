0076e342b349a3c21f323c0085eac0924dfcc362
Ankur Khandelwal <khandelwalankur176@gmail.com>
2022-06-01 21:10:53 +0530

    add routes and controllers

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,16 @@
 const router = require("express").Router();
+const {signup, login, allCourses, fetchCourse, uploadCourse, deleteCourse, updateCourse, buyCourse, search} = require("./controllers");
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/allCourses', allCourses);
+router.get('/course/:id', fetchCourse);
+router.post('/uploadCourse', uploadCourse);
+router.post('/deleteCourse', deleteCourse);
+router.post('/updateCourse', updateCourse);
+router.post('/buyCourse', buyCourse);
+router.get('/search', search);
+
 
 
 
